742b76fa4999c437b63aefd17610fa6799f7efc6
maheenur13 <differentmahee13@gmail.com>
2022-06-24 01:27:52 +0600

    add origin

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,7 @@ import { wrapper } from '@store';
 import type { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+	return <Component {...pageProps} />;
 }
 
 export default wrapper.withRedux(MyApp);
